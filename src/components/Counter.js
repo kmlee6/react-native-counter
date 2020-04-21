7c77c7a342ab72b400dc5e428e8edb0703eb5ff6
Kelvin Lee <kelvin.lee@oocl.com>
2020-04-21 14:08:47 +0800

    add increase button

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,15 +4,21 @@ export default class Counter extends Component {
   constructor(props) {
     super(props);
 
+    this.onIncrease = this.onIncrease.bind(this);
+
     this.state = {
       number: 0,
     };
   }
 
+  onIncrease() {
+    this.setState((prevState) => ({ number: prevState.number + 1 }));
+  }
+
   render() {
     return (
       <div>
-        <button>+</button>
+        <button onClick={this.onIncrease}>+</button>
         <span>{this.state.number}</span>
         <button>-</button>
       </div>
